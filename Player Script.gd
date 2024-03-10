0ad7fc9f19bd3f2b36b03ca812edbbaa2cda00b1
Dimmer <nathandimmer@gmail.com>
2024-03-10 10:57:14 -0400

    End of Stream 2

--- Player Script.gd
+++ Player Script.gd
@@ -7,28 +7,37 @@ extends CharacterBody3D
 var _working_velocity := Vector3.ZERO
 
 @onready var _spring_arm: SpringArm3D = $SpringArm3D
-@onready var _model: MeshInstance3D = $CharModel
+@onready var _model: Node3D = $CharModel
 
 func _physics_process(delta):
 	var move_direction := Vector3.ZERO
 	
 	move_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	move_direction.y = Input.get_action_strength("back") - Input.get_action_strength("forward")
+	move_direction.z = Input.get_action_strength("back") - Input.get_action_strength("forward")
 	move_direction = move_direction.rotated(Vector3.UP, _spring_arm.rotation.y).normalized()
 	
 	_working_velocity.x = move_direction.x * speed
 	_working_velocity.z = move_direction.z * speed
-	_working_velocity.y -= gravity * delta
+	
+	if is_on_floor():
+		_working_velocity.y = 0
+	else:
+		_working_velocity.y -= gravity * delta
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		_working_velocity.y = jump_strength
 	
 	set_velocity(_working_velocity)
+	print(get_velocity())
 	move_and_slide()
+	print(get_real_velocity())
 	
 	if get_velocity().length() > .2:
 		var look_direction = Vector2(get_velocity().z, get_velocity().x)
 		_model.rotation.y = look_direction.angle()
 
 func _process(delta):
-	_spring_arm.position = position
+	_spring_arm.position = Vector3(position.x, position.y + 1.0, position.z)
 
 
 
---
+++ wuhu_island/ReadMe.txt
@@ -0,0 +1,5 @@
+Wuhu Island created by Nintendo
+Ported to Blender by GlitchingNo.
+Credit not necessary, but appreciated
+
+Big Thank You to JimmyKaz who helped me with a lot of problems I had while porting the Model to Blender
